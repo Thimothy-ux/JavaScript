7e2af22c8cfadeba63844103d602610185840911
Thimothy <potluri.thimothy42@gmail.com>
2024-01-06 15:36:26 +0530

    Adding Varible file

--- Varibles.js
+++ Varibles.js
@@ -27,6 +27,41 @@
 // // Symbol : Unique and cant be changed.
 
 
-// Comments in js : // , /* */
+// // Comments in js : // , /* */
 
-// To check data-type  : typeof
+// // To check data-type  : typeof
+
+//  // Declaring varibles : 
+
+// // Var , let const 
+
+/* 
+   Var is Global scope 
+   Let and const is Block Scope.
+   let and const can't be Hoisted.
+*/
+
+// Operator presedence : ()
+
+// // Template Literal : 
+
+// let name = "Thimothy";
+// console.log(`My name is ${name}`)
+
+
+// // Type Conversion : We manually convert datatype. String(1) , Number('1')
+// // Type Coercion : Js Automatically converts the datatypes. 
+   
+//   var s = '10' + 10;
+//   console.log(s)                
+//   console.log(typeof(s))
+
+// // Falsy values : 0 , '' , undefined , null , NaN(Not a Number)
+
+// console.log(Boolean(0))
+   
+// // == : It checks only the data, not the data type.
+// // === : It checks both data type and value.
+
+// // Ternary operators: condition ? statement : statement
+
