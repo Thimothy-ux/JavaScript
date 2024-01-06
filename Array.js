6685385714a2cc732e7cebc249e2af71e087dde3
Thimothy <potluri.thimothy42@gmail.com>
2024-01-06 16:21:26 +0530

    Adding Arrays file

---
+++ Array.js
@@ -0,0 +1,12 @@
+// const friends = ['Michel', 'Steven', 'Peter'];
+// console.log(friends)
+
+// // Array : It is collection of Similar elements but not in js.
+
+// let data = [ 'Thimothy' , 2 ]
+// console.log(data)
+
+// //  Another way to create the array
+// let c = new Array(12,"Mango",24)
+// console.log(c)
+
