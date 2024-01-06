4131a1253fc24351614875c9355cec8076aef2c7
Thimothy <potluri.thimothy42@gmail.com>
2024-01-06 15:12:58 +0530

    Adding Varible file

---
+++ Varibles.js
@@ -0,0 +1,29 @@
+// // JS has primitive and Object type values.
+
+// // Primitives
+
+// // Number : Decimals and Integers.
+
+// var a = 24;
+// console.log(a)
+
+// // String : Used for text(Group of Characters).
+
+// var name  = "Thimothy"
+// console.log(name)
+
+// // Boolean : Ture or false.
+
+// // Undifined : empty value.
+
+// var b;
+// console.log(b)
+
+// // null : Unknown value.
+
+// var val  = null;
+// console.log(val)
+
+// // Symbol : Unique and cant be changed.
+
+
