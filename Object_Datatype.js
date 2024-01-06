f820f77baccb4fc6f5c132b56b39b296ecd0ba6d
Thimothy <potluri.thimothy42@gmail.com>
2024-01-06 17:14:40 +0530

    Adding Object Datatype file

---
+++ Object_Datatype.js
@@ -0,0 +1,14 @@
+// Object : It provides un-Ordered data.
+
+// const Tom = {
+//     firstName : "Thimothy",
+//     lastName : "Potluri",
+//     friends : ['George bush', 'Games bond']
+// }
+
+// console.log(Tom)
+// console.log(Tom.firstName)
+// console.log(Tom['firstName'])
+
+// const nameKey = "Name"
+// console.log(Tom['first'+nameKey])
