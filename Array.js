60d576d288a85023dd7d2e7b1d3cf0daa5ab80d8
Thimothy <potluri.thimothy42@gmail.com>
2024-01-06 16:50:53 +0530

    Adding more Array functions

--- Array.js
+++ Array.js
@@ -7,6 +7,51 @@
 // console.log(data)
 
 // //  Another way to create the array
-// let c = new Array(12,"Mango",24)
-// console.log(c)
 
+// const  c = new Array(12,"Mango",24)
+// console.log(c)
+// console.log(c.length) // no of  values in array.
+
+/*
+Note : 
+  With CONST declaration, 
+     We can change the values in array but we can't initialized the entire array.
+*/
+
+
+// // ---> Basic Array Operations :
+
+let val = [1,2,3,4]
+
+// // Push : To Add New value at  last.
+
+// val.push(5)
+
+// // unshift : To add New Value at first.
+
+// val.unshift(6)
+
+// // pop : To remove value at the last.
+
+// val.pop()
+
+// // shift : To remove value at the begining.
+
+// val.shift()
+
+// // indexof() : To know the index of particular value.
+ 
+// console.log(val.indexOf(3))
+
+// includes() : Checks whether the particular value is there inside the array. 
+
+
+// console.log(val.includes(2))
+
+//// concat : Add more than one array into single array.
+
+// const newArr = val.concat([4,5,6,7])
+// console.log(newArr)
+
+
+console.log(val)
