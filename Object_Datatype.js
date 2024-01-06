770d63edef47fc8e30d0704ee5e1eb91aab7fc86
Thimothy <potluri.thimothy42@gmail.com>
2024-01-06 17:31:37 +0530

    Adding loop breaking keywords

--- Object_Datatype.js
+++ Object_Datatype.js
@@ -26,13 +26,28 @@
 
 // // Loops : for , while
 
+// // Continue : to skip the current iteration.
+// // break : to come out of the loop.
+
 // for(let i = 1; i<= 10; i++)
 // {
+//     if(i == 4){
+//         continue;
+//     }
+
 //     console.log(i)
+
 // }
 
-let i = 1
-while(i <= 10){
-    console.log(i);
-    i++;
-}
+
+
+// let i = 1
+// while(i <= 10){
+
+//        if(i > 5){
+//         break;
+//        }
+
+//     console.log(i);
+//     i++;
+// }
