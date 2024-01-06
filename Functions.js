013e9dd059665daeb3a4f68ebc06aefee97a8cc8
Thimothy <potluri.thimothy42@gmail.com>
2024-01-06 15:44:51 +0530

    Adding functions file

---
+++ Functions.js
@@ -0,0 +1,31 @@
+// // Normal function : 
+
+// function result()
+// {
+//     let val = 10 + 10;
+//     return val;
+// }
+
+// const sum = result();
+// console.log(sum)
+
+
+// // Anonymous function : 
+
+// const fn = function(){
+//     let val = 10 + 10;
+//     return val;
+// }
+
+// const data = fn()
+// console.log(data)
+
+// // Arrow function
+
+// const result = () => { 
+//      let val = 10 + 10;
+//      return val;
+// }
+
+// const total = result()
+// console.log(total)
